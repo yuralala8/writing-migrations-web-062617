9a9feed31cab1e89f99c2e81711a07a043bb66c9
yuralala8 <yc1435@nyu.edu>
2017-07-07 11:40:08 -0400

    writing migrations

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,8 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+
+end
